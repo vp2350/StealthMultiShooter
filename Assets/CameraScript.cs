9c72da3518dad3c1402f162eb313d832f3158bc6
Vansh Pahuja <vp2350@rit.edu>
2021-06-16 10:59:48 -0400

    Added player model and movement

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] GameObject character;
+    [SerializeField] [Range(0f, 1f)] float minMouseDisplacementRadius = .25f;
+    [SerializeField] [Range(1f, 10f)] float displacementScale = 2f;
+    void Start()
+    {
+        GetComponents();
+    }
+
+    void Update()
+    {
+
+        Vector3 target = new Vector3(character.transform.position.x,
+                                     character.transform.position.y,
+                                     transform.position.z);
+
+        transform.position = target;
+    }
+
+    void GetComponents()
+    {
+        if (character == null)
+        {
+            character = GameObject.Find("PlayerChar");
+        }
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] [Range(5, 50)] float maxWalkSpeed = 150f;
+    [SerializeField] bool accelerationEnabled = true;
+    [SerializeField] [Range(1, 200)] float accelerationpercent = 110f;
+    float acceleration, speed;
+    Vector2 move;
+
+    void Start()
+    {
+        acceleration = (accelerationpercent / 100f) * maxWalkSpeed;
+        GetComponents();
+    }
+
+    void Update()
+    {
+        RotateSprite();
+        UpdateMoveControls();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMove();
+    }
+
+    #region updates
+    void RotateSprite()
+    {
+        // Player can control direction
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //get mouse position
+
+        Vector3 direction = transform.position - mousePos; //get vector from position to mouse pos
+        float angleOfRotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90; //get angle
+        transform.localRotation = Quaternion.Euler(0, 0, angleOfRotation); //set rotation
+    }
+
+    void UpdateMove()
+    {
+
+
+        if (accelerationEnabled)
+        { //accelearte if enabled
+            if (move.magnitude > 0) speed = Mathf.Min(speed + acceleration * Time.deltaTime, maxWalkSpeed);
+            else speed = Mathf.Max(speed - acceleration * Time.deltaTime, 0f);
+        }
+        else
+        { //otherwize go to max speed
+            speed = maxWalkSpeed;
+        }
+
+        move = move * speed * Time.deltaTime; //multiply by speed and time
+        Vector2 target = new Vector2(transform.position.x + move.x, transform.position.y + move.y); //add to move
+        rb.MovePosition(target); //move rigidbody
+    }
+
+    void UpdateMoveControls()
+    {
+        float x = 0, y = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            x = -4;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = 4;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            y = 4;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            y = -4;
+        }
+
+        move = new Vector2(x, y); //turn into vector
+        move = move.normalized; //normalize
+    }
+    #endregion
+
+    void GetComponents()
+    {
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+    }
+}
